47f3b4a271d7758e5d870e2b03ddc89352be9d32
Joe Wass <jwass@crossref.org>
2018-03-26 14:03:28 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject event-data-query "0.2.6"
+(defproject event-data-query "0.2.7"
   :description "Event Data Query"
   :url "http://eventdata.crossref.org/"
   :license {:name "MIT License"
